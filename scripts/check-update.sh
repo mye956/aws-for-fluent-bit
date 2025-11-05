d87fa03f717e9e761b5ec57c69b861bf4238799f
Michael Ye <yemike@amazon.com>
2025-11-05 23:52:37 +0000

    added script to check if there's a new AL2 image

---
+++ scripts/check-update.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Pull AL2 image from public ECR
+docker pull public.ecr.aws/amazonlinux/amazonlinux:2
+
+# Get image SHA
+IMAGE_SHA=$(docker inspect --format='{{index .RepoDigests 0}}' public.ecr.aws/amazonlinux/amazonlinux:2)
+
+echo "Image SHA: $IMAGE_SHA"
+
+CURRENT_IMAGE_SHA=$(head -n 1 ../dummy.txt)
+
+echo "Current image SHA: $CURRENT_IMAGE_SHA"
+
+if [[ "$IMAGE_SHA" == "$CURRENT_IMAGE_SHA" ]]; then
+    echo "No new base amazon linux image"
+else 
+    echo "There is a new base amazon linux image"
+fi
+
+# Clean up the pulled image
+docker rmi public.ecr.aws/amazonlinux/amazonlinux:2
+
