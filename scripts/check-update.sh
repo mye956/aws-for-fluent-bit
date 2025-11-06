972d87fcc61ef3c27653fcf1a1bb778936f403fd
Michael Ye <yemike@amazon.com>
2025-11-06 01:18:21 +0000

    updating to modify linux.version

--- scripts/check-update.sh
+++ scripts/check-update.sh
@@ -9,38 +9,63 @@ cleanup() {
 
 trap cleanup EXIT
 
-jq -c '.[]' ./linux.version | while read -r entry; do
-    echo "$entry" | jq '.linux'
-    sha=$(echo "$entry" | jq -r '.linux."amazon-linux-sha" // empty')
-    tag=$(echo "$entry" | jq -r '.linux."al-tag"')
-    if [[ -z "$sha" ]]; then
-        echo "ERROR: Can't parse sha in linux.version"
-        exit 1
-    fi
+echo "Before: $(cat linux.version)"
 
-    if [[ -z "$tag" ]]; then
-        echo "ERROR: Can't parse tag in linux.version"
-        exit 1
-    fi
+# Get indices and iterate
+for i in $(jq 'keys[]' linux.version); do
+    current_sha=$(jq -r ".[$i].linux.\"amazon-linux-sha\"" linux.version)
+    tag=$(jq -r ".[$i].linux.\"al-tag\"" linux.version)
+    echo "Index $i: $current_sha"
 
-    echo "Extracted SHA: $sha"
-    echo "Extracted tag: $tag"
+    docker pull "public.ecr.aws/amazonlinux/amazonlinux:$tag" 
+    IMAGE_SHA=$(docker inspect --format='{{index .RepoDigests 0}}' public.ecr.aws/amazonlinux/amazonlinux:$tag)
+    echo "$tag Image SHA: $IMAGE_SHA"
 
-    # Pull AL2 image from public ECR
-    docker pull "public.ecr.aws/amazonlinux/amazonlinux:$tag"
+    echo "Current Image SHA: $current_sha"
 
-    # Get image SHA
-    IMAGE_SHA=$(docker inspect --format='{{index .RepoDigests 0}}' public.ecr.aws/amazonlinux/amazonlinux:2)
-
-    echo "Image SHA: $IMAGE_SHA"
-    echo "Current image SHA: $sha"
-
-    if [[ "$IMAGE_SHA" == "$sha" ]]; then
-        echo "No new base amazon linux image"
-    else 
-        echo "There is a new base amazon linux image"
-    fi
+    if [[ "$IMAGE_SHA" == "$current_sha" ]]; then
+        echo "No new base amazon linux image for $tag"
+    else
+        # Modify specific index
+        echo "There is a new base amazon linux image for $tag. Updating linux.version"
+        jq ".[$i].linux.\"amazon-linux-sha\" = \"$IMAGE_SHA\"" linux.version > tmp.json && mv tmp.json linux.version
+    fi 
 done
+
+echo "After: $(cat linux.version)"
+
+# jq -c '.[]' ./linux.version | while read -r entry; do
+#     echo "$entry" | jq '.linux'
+#     sha=$(echo "$entry" | jq -r '.linux."amazon-linux-sha" // empty')
+#     tag=$(echo "$entry" | jq -r '.linux."al-tag"')
+#     if [[ -z "$sha" ]]; then
+#         echo "ERROR: Can't parse sha in linux.version"
+#         exit 1
+#     fi
+
+#     if [[ -z "$tag" ]]; then
+#         echo "ERROR: Can't parse tag in linux.version"
+#         exit 1
+#     fi
+
+#     echo "Extracted SHA: $sha"
+#     echo "Extracted tag: $tag"
+
+#     # Pull AL2 image from public ECR
+#     docker pull "public.ecr.aws/amazonlinux/amazonlinux:$tag"
+
+#     # Get image SHA
+#     IMAGE_SHA=$(docker inspect --format='{{index .RepoDigests 0}}' public.ecr.aws/amazonlinux/amazonlinux:2)
+
+#     echo "Image SHA: $IMAGE_SHA"
+#     echo "Current image SHA: $sha"
+
+#     if [[ "$IMAGE_SHA" == "$sha" ]]; then
+#         echo "No new base amazon linux image"
+#     else 
+#         echo "There is a new base amazon linux image"
+#     fi
+# done
 
 
 
