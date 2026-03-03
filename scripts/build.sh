5c6639a0dfb83e60bbebb27d477c5383edcddc9d
Michael Ye <yemike@amazon.com>
2026-03-03 21:35:27 +0000

    enable debugging mode in build scriopt

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,7 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-set -e
+set -ex
 
 # Validate BUILD_VERSION variable is set
 if [ "$BUILD_VERSION" != "2" ] && [ "$BUILD_VERSION" != "3" ]; then
